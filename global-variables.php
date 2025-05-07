2b74c5eb3ce20a42da764d1ee434f8bc2f55b7d1
Mqroon <bobbyrobby504+github@gmail.com>
2025-05-07 10:27:24 -0500

    github plugin uri

--- global-variables.php
+++ global-variables.php
@@ -19,6 +19,7 @@
  * Version:           1.0.0
  * Author:            Walker Alexander
  * Author URI:        https://github.com/Mqroon/
+ * GitHub Plugin URI: https://github.com/Mqroon/WP-Global-Variables.git
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       global-variables
